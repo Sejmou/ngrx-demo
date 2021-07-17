eb16607512950a17ed9aaea14d14a0e83814a938
Sejmou <samo.kolter@gmail.com>
2021-07-17 14:18:04 +0200

    cleanup

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -6,11 +6,9 @@ import {
   OnInit
 } from '@angular/core';
 import { NgForm } from '@angular/forms';
-import { Router } from '@angular/router';
-import { Observable, Subscription } from 'rxjs';
+import { Subscription } from 'rxjs';
 import { Store } from '@ngrx/store';
 
-import { AuthService, AuthResponseData } from './auth.service';
 import { AlertComponent } from '../shared/alert/alert.component';
 import { PlaceholderDirective } from '../shared/placeholder/placeholder.directive';
 import * as fromApp from '../store/app.reducer';
@@ -30,8 +28,6 @@ export class AuthComponent implements OnInit, OnDestroy {
   private storeSub: Subscription;
 
   constructor(
-    private authService: AuthService,
-    private router: Router,
     private componentFactoryResolver: ComponentFactoryResolver,
     private store: Store<fromApp.AppState>
   ) { }
@@ -64,13 +60,6 @@ export class AuthComponent implements OnInit, OnDestroy {
 
     form.reset();
   }
-
-  // not good, state would be duplicated, should be managed in store too!
-  // we should create a clearError Action or something like that in this case
-  // won't do it as this method isn't called at all anyway
-  // onHandleError() {
-  //   this.error = null;
-  // }
 
   ngOnDestroy() {
     if (this.closeSub) {
